f060e703ccd4cab0db4ad5df154b04617623bb57
9NEAR7 <instantcrushxx97@gmail.com>
2022-11-07 19:25:59 -0600

    insercion de multiples registros a la bd

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import axios, {AxiosInstance} from 'axios';
+import { Model } from 'mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokeResponse } from '../../dist/seed/interfaces/poke-response.interface';
 
 
@@ -8,19 +11,36 @@ export class SeedService {
 
   private readonly axios: AxiosInstance = axios;
 
+  constructor(
+
+    @InjectModel(Pokemon.name)
+    private readonly pokeModel: Model<Pokemon>
+  ){}
+
   async executeSeed(){
 
+    await this.pokeModel.deleteMany({});
+
     const {data} = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+
+    const pokemonToInsert: {name: string, no: number}[] = [];
 
     data.results.forEach(({name, url}) =>{
 
       const segments = url.split('/');
       const no = +segments[segments.length - 2];
 
-      console.log({name,no});
-    })
+      //const pokemon = await this.pokeModel.create({name,no});
 
-    return data.results;
+     pokemonToInsert.push({name, no});
+      
+      
+    });
+
+    await this.pokeModel.insertMany(pokemonToInsert);
+    //insert into pokemons(name,no)
+
+    return 'SEED EXECUTED';
   }
 
 }
